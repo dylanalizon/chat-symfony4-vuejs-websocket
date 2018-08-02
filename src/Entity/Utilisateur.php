2e84d1beb954ebea6077bf8d4fd9fdadb488fdf7
Dylan <dylan@dev-alizon.fr>
2018-08-02 15:02:17 +0200

    Installation FOSUserBundle

---
+++ src/Entity/Utilisateur.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UtilisateurRepository")
+ */
+class Utilisateur extends BaseUser
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
